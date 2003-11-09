a1da104dc70a968d6c586fab06696c623ce54644
troyer <troyer@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2003-11-09 20:21:01 +0000

    *** empty log message ***
    git-svn-id: svn://localhost:7744/trunk/alps@548 3665984a-f8b0-4bb5-b30c-cdcaecbe7ced

--- example/scheduler/ising2.h
+++ example/scheduler/ising2.h
@@ -7,15 +7,22 @@
 *
 * Copyright (C) 1994-2003 by Matthias Troyer <troyer@itp.phys.ethz.ch>,
 *
-* This program is free software; you can redistribute it and/or
-* modify it under the terms of the GNU General Public License
-* as published by the Free Software Foundation; either version 2
-* of the License, or (at your option) any later version.
+* This software is part of the ALPS library, published under the 
+* ALPS Library License; you can use, redistribute it and/or modify 
+* it under the terms of the License, either version 1 or (at your option) 
+* any later version.
 *
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
+* You should have received a copy of the ALPS Library License along with 
+* the ALPS Library; see the file License.txt. If not, the license is also 
+* available from http://alps.comp-phys.org/. 
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+* FITNESS FOR A PARTICULAR PURPOSE, TITLE AND NON-INFRINGEMENT. IN NO EVENT 
+* SHALL THE COPYRIGHT HOLDERS OR ANYONE DISTRIBUTING THE SOFTWARE BE LIABLE 
+* FOR ANY DAMAGES OR OTHER LIABILITY, WHETHER IN CONTRACT, TORT OR OTHERWISE, 
+* ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+* DEALINGS IN THE SOFTWARE.
 *
 **************************************************************************/
 
---
+++ test/model/example11.C
@@ -0,0 +1,64 @@
+/***************************************************************************
+* PALM++/model library
+*
+* example/example1.C
+*
+* $Id$
+*
+* Copyright (C) 2003 by Matthias Troyer <troyer@itp.phys.ethz.ch>
+*
+* This software is part of the ALPS library, published under the 
+* ALPS Library License; you can use, redistribute it and/or modify 
+* it under the terms of the License, either version 1 or (at your option) 
+* any later version.
+*
+* You should have received a copy of the ALPS Library License along with 
+* the ALPS Library; see the file License.txt. If not, the license is also 
+* available from http://alps.comp-phys.org/. 
+
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+* FITNESS FOR A PARTICULAR PURPOSE, TITLE AND NON-INFRINGEMENT. IN NO EVENT 
+* SHALL THE COPYRIGHT HOLDERS OR ANYONE DISTRIBUTING THE SOFTWARE BE LIABLE 
+* FOR ANY DAMAGES OR OTHER LIABILITY, WHETHER IN CONTRACT, TORT OR OTHERWISE, 
+* ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+* DEALINGS IN THE SOFTWARE.
+*
+**************************************************************************/
+
+#include <alps/model.h>
+#include <iostream>
+
+int main()
+{
+
+#ifndef BOOST_NO_EXCEPTIONS
+  try {
+#endif
+    alps::Parameters parms;
+    std::cin >> parms;
+    alps::ModelLibrary models(parms);
+    alps::graph_factory<> lattices(parms);
+    alps::HamiltonianDescriptor<short> ham(models.hamiltonian(parms["MODEL"]));
+    parms.copy_undefined(ham.default_parameters());
+    ham.set_parameters(parms);
+    alps::BasisStatesDescriptor<short> basis(ham.basis(),lattices.graph());
+    for (int i=0;i<basis.basis().constraints().size();++i)
+      std::cout << "Constraint: " << basis.basis().constraints()[i].first << "=" 
+                <<  basis.basis().constraints()[i].second << std::endl;
+
+#ifndef BOOST_NO_EXCEPTIONS
+}
+catch (std::exception& e)
+{
+  std::cerr << "Caught exception: " << e.what() << "\n";
+  exit(-1);
+}
+catch (...)
+{
+  std::cerr << "Caught unknown exception\n";
+  exit(-2);
+}
+#endif
+}
