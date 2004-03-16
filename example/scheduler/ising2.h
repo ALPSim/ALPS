085b0089f7236b02911fb32aff37e10b2ed48103
wistaria <wistaria@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2004-03-16 15:12:31 +0000

    *** empty log message ***
    git-svn-id: svn://localhost:7744/trunk/alps@691 3665984a-f8b0-4bb5-b30c-cdcaecbe7ced

--- example/scheduler/ising2.h
+++ example/scheduler/ising2.h
@@ -1,20 +1,19 @@
-/***************************************************************************
-* PALM++/scheduler example
+/*****************************************************************************
 *
-* scheduler/ising.h an example Ising model simulation
+* ALPS Project: Algorithms and Libraries for Physics Simulations
 *
-* $Id$
+* ALPS Libraries
 *
-* Copyright (C) 1994-2003 by Matthias Troyer <troyer@itp.phys.ethz.ch>,
+* Copyright (C) 1994-2003 by Matthias Troyer <troyer@itp.phys.ethz.ch>
 *
-* This software is part of the ALPS library, published under the 
-* ALPS Library License; you can use, redistribute it and/or modify 
-* it under the terms of the License, either version 1 or (at your option) 
-* any later version.
-*
-* You should have received a copy of the ALPS Library License along with 
-* the ALPS Library; see the file License.txt. If not, the license is also 
-* available from http://alps.comp-phys.org/. 
+* This software is part of the ALPS libraries, published under the ALPS
+* Library License; you can use, redistribute it and/or modify it under
+* the terms of the license, either version 1 or (at your option) any later
+* version.
+* 
+* You should have received a copy of the ALPS Library License along with
+* the ALPS Libraries; see the file LICENSE.txt. If not, the license is also
+* available from http://alps.comp-phys.org/.
 *
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
@@ -24,7 +23,9 @@
 * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
 * DEALINGS IN THE SOFTWARE.
 *
-**************************************************************************/
+*****************************************************************************/
+
+/* $Id$ */
 
 //=======================================================================
 // This file defines the simulation specific classes for a simple
@@ -35,11 +36,11 @@
 #include <alps/lattice.h>
 
 typedef boost::adjacency_list<boost::vecS,boost::vecS,boost::undirectedS,
-			      // vertex property
-			      boost::property<alps::vertex_type_t,int>,
-			      // edge property
+                              // vertex property
+                              boost::property<alps::vertex_type_t,int>,
+                              // edge property
                               boost::property<alps::edge_type_t,int>
-			      > graph_type;
+                              > graph_type;
 
 class IsingSimulation2 : public alps::scheduler::LatticeMCRun<graph_type>
 {
--- tool/compactrun.C
+++ tool/compactrun.C
@@ -1,22 +1,19 @@
-/***************************************************************************
-* ALPS++/scheduler library
+/*****************************************************************************
 *
-* tool/compactrun.C   compact a run checkpoint 
+* ALPS Project: Algorithms and Libraries for Physics Simulations
 *
+* ALPS Libraries
 *
-* $Id$
+* Copyright (C) 2002-2003 by Matthias Troyer <troyer@itp.phys.ethz.ch>
 *
-* Copyright (C) 2002-2003 by Matthias Troyer <troyer@itp.phys.ethz.ch>,
-*
-* This software is part of the ALPS library, published under the 
-* ALPS Library License; you can use, redistribute it and/or modify 
-* it under the terms of the License, either version 1 or (at your option) 
-* any later version.
-*
-* You should have received a copy of the ALPS Library License along with 
-* the ALPS Library; see the file License.txt. If not, the license is also 
-* available from http://alps.comp-phys.org/. 
-
+* This software is part of the ALPS libraries, published under the ALPS
+* Library License; you can use, redistribute it and/or modify it under
+* the terms of the license, either version 1 or (at your option) any later
+* version.
+* 
+* You should have received a copy of the ALPS Library License along with
+* the ALPS Libraries; see the file LICENSE.txt. If not, the license is also
+* available from http://alps.comp-phys.org/.
 *
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
@@ -26,7 +23,9 @@
 * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
 * DEALINGS IN THE SOFTWARE.
 *
-**************************************************************************/
+*****************************************************************************/
+
+/* $Id$ */
 
 #include <alps/scheduler.h>
 #include <boost/filesystem/operations.hpp>
@@ -51,7 +50,7 @@ try {
   boost::filesystem::path writepath = (make_backup ? outpath.branch_path()/(outpath.leaf()+".bak") : outpath);
   
   std::cout << "Compacting run file " << inname << " to " <<  outname
-	    <<std::endl;
+            <<std::endl;
 
   { // scope for life time of files
     alps::IXDRFileDump in(inpath);
