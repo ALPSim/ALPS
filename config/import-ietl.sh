4d59f9babd4bc76079acb39605928739468ae094
wistaria <wistaria@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2004-08-20 12:14:24 +0000

    *** empty log message ***
    git-svn-id: svn://localhost:7744/trunk/alps@1130 3665984a-f8b0-4bb5-b30c-cdcaecbe7ced

---
+++ config/import-ietl.sh
@@ -0,0 +1,77 @@
+#!/bin/sh
+
+branch=ietl
+taghead=IETL
+checkfile=ietl/lanczos.h
+
+dir=src
+files='ietl'
+files_remove='ietl/Makefile.in'
+import_option="-m '' -ko"
+
+srcdir=$1
+tmpdir=import.$$
+
+this=`basename $0`
+if test -f "$this"; then :; else
+  echo "ERROR: this script should be run in the config directory" && exit -1
+fi
+if test -z "$srcdir"; then
+  echo "ERROR: $0 src_dir" && exit -1
+fi
+if test -f "../$dir/$checkfile"; then :; else
+  echo "ERROR: ../$dir/$checkfile not found" && exit -1
+fi
+if test -f "$srcdir/$checkfile"; then :; else
+  echo "ERROR: import source files ($checkfile) not found" && exit -1
+fi
+
+ROOT=`cat ../$dir/CVS/Root`
+REPOSITORY=`cat ../$dir/CVS/Repository`
+echo "CVS root = $ROOT"
+echo "CVS repository = $REPOSITORY"
+
+OLD_TAG=`(cd ../$dir && cvs status -v $checkfile | grep $taghead | head -1 | awk '{print $1}')`
+echo -n "Tag for previously imported source files [$OLD_TAG] ? : "
+read answer &> /dev/null
+if test -n "$answer"; then
+  OLD_TAG="$answer"
+fi
+if test -z "$OLD_TAG"; then
+  echo "ERROR: empty tag." && exit -1
+fi
+
+NEW_TAG=${taghead}_`date +'%Y%m%d'`
+echo -n "Tag for newly imported source files [$NEW_TAG] ? : "
+read answer &> /dev/null
+if test -n "$answer"; then
+  NEW_TAG="$answer"
+fi
+
+if test "$NEW_TAG" = "$OLD_TAG"; then
+  echo "ERROR: tag $NEW_TAG already exists" && exit -1
+fi
+
+mkdir $tmpdir
+
+echo "Copying files into $tmpdir..."
+for i in $files; do
+  cp -rp $srcdir/$i $tmpdir/
+done
+for i in $files_remove; do
+  rm -f $tmpdir/$i
+done
+
+echo "Converting preambles..."
+find $tmpdir -type f -name '*\.h' -or -name '*\.C' -or -name '*\.hpp' -or -name '*\.h\.in' | xargs ./update_preamble
+
+set -x
+
+# (cd $tmpdir && cvs -d ${ROOT} import ${import_option} ${REPOSITORY} ${branch} ${NEW_TAG})
+echo "(cd $tmpdir && cvs -d ${ROOT} import ${import_option} ${REPOSITORY} ${branch} ${NEW_TAG})"
+
+# (cd ../$dir && cvs up -j $OLD_TAG -j $NEW_TAG)
+
+set +x
+
+# rm -rf $tmpdir
