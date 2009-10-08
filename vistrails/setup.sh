868dfa0c9ad136dadd3e852a0a8c3209b6e5c20b
troyer <troyer@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2009-10-08 07:34:14 +0000

    Fixed capitalization bug in symbolic link
    git-svn-id: svn://localhost:7744/trunk/alps@3291 3665984a-f8b0-4bb5-b30c-cdcaecbe7ced

--- vistrails/setup.sh
+++ vistrails/setup.sh
@@ -1,5 +1,5 @@
-ln -s $PWD/subworkflows/ShowAsHtml.xml ~/.vistrails/subworkflows/ShowAsHtml.xml
+ln -s $PWD/subworkflows/ShowAsHTML.xml ~/.vistrails/subworkflows/ShowAsHTML.xml
 ln -s $PWD/subworkflows/PersistentResults.xml ~/.vistrails/subworkflows/PersistentResults.xml
 ln -s $PWD/userpackages/alps ~/.vistrails/userpackages/alps
-rmdir -f ~/.vistrails/persistent_files
+rmdir ~/.vistrails/persistent_files
 ln -s $PWD/persistent_files ~/.vistrails/persistent_files
