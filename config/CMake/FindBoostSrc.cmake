1135d2f4c24cfab1c17e03c893298a0f6ea6c15f
gamperl <gamperl@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2009-11-12 18:20:48 +0000

    addes somme missing cmake files
    git-svn-id: svn://localhost:7744/trunk/alps@3332 3665984a-f8b0-4bb5-b30c-cdcaecbe7ced

---
+++ config/CMake/FindBoostSrc.cmake
@@ -0,0 +1,60 @@
+# - FindBoostSrc
+# Find Boost source tree
+#
+#
+#
+
+if(BOOST_ROOT)
+  find_path(Boost_ROOT_DIR "libs/filesystem/src/path.cpp" ${BOOST_ROOT})
+else(BOOST_ROOT)
+  set(DIR0
+    $ENV{HOME} $ENV{HOME}/src $ENV{HOME}/ALPS/src
+    "$ENV{HOMEDRIVE}/Program Files"
+    "$ENV{HOMEDRIVE}$ENV{HOMEPATH}/My Documents"
+    "$ENV{HOMEDRIVE}$ENV{HOMEPATH}/My Documents/src"
+    "$ENV{HOMEDRIVE}$ENV{HOMEPATH}/My Documents/ALPS/src")
+  set(DIR1 boost boostsrc boost_1_40_0 boost_1_39_0 boost_1_38_0 boostsrc_1_38_0)
+  set(_boost_SEARCH_PATH "")
+  foreach(D0 ${DIR0})
+    foreach(D1 ${DIR1})
+      set(_boost_SEARCH_PATH ${_boost_SEARCH_PATH} ${D0}/${D1})
+    endforeach(D1)
+  endforeach(D0)
+  find_path(Boost_ROOT_DIR "libs/filesystem/src/path.cpp" ${_boost_SEARCH_PATH})
+endif(BOOST_ROOT)
+
+if(Boost_ROOT_DIR)
+  set(Boost_INCLUDE_DIR ${Boost_ROOT_DIR} CACHE PATH "Boost Include Directory")
+endif(Boost_ROOT_DIR)
+
+# check Boost version (from Modules/FindBoost.cmake)
+if(Boost_INCLUDE_DIR)
+  set(_boost_VERSION 0)
+  set(_boost_LIB_VERSION "")
+  file(READ "${Boost_INCLUDE_DIR}/boost/version.hpp" _boost_VERSION_HPP_CONTENTS)
+  string(REGEX REPLACE ".*#define BOOST_VERSION ([0-9]+).*" "\\1" _boost_VERSION "${_boost_VERSION_HPP_CONTENTS}")
+  string(REGEX REPLACE ".*#define BOOST_LIB_VERSION \"([0-9_]+)\".*" "\\1" _boost_LIB_VERSION "${_boost_VERSION_HPP_CONTENTS}")
+  set(Boost_LIB_VERSION ${_boost_LIB_VERSION} CACHE INTERNAL "The library version string for boost libraries")
+  set(Boost_VERSION ${_boost_VERSION} CACHE INTERNAL "The version number for boost libraries")
+  if(NOT "${Boost_VERSION}" STREQUAL "0")
+    MATH(EXPR Boost_MAJOR_VERSION "${Boost_VERSION} / 100000")
+    MATH(EXPR Boost_MINOR_VERSION "${Boost_VERSION} / 100 % 1000")
+    MATH(EXPR Boost_SUBMINOR_VERSION "${Boost_VERSION} % 100")
+  endif(NOT "${Boost_VERSION}" STREQUAL "0")
+endif(Boost_INCLUDE_DIR)
+
+if(Boost_ROOT_DIR)
+  message(STATUS "Found Boost Source: ${Boost_ROOT_DIR}")
+endif(Boost_ROOT_DIR)
+
+set(BUILD_BOOST_DATE_TIME TRUE)
+set(BUILD_BOOST_FILESYSTEM TRUE)
+set(BUILD_BOOST_PROGRAM_OPTIONS TRUE)
+set(BUILD_BOOST_REGEX TRUE)
+# set(BUILD_BOOST_SERIALIZATION TRUE)
+set(BUILD_BOOST_SYSTEM TRUE)
+
+# Boost ライブラリの auto link を防ぐ
+add_definitions(-DBOOST_ALL_NO_LIB=1)
+
+mark_as_advanced(Boost_INCLUDE_DIR)
---
+++ config/CMake/FindLPSolve.cmake
@@ -0,0 +1,43 @@
+# - Find LPSolve
+# Find LPSolve headers and libraries.
+#
+#  LPSolve_FOUND        - True if LPSolve found
+#  LPSolve_INCLUDE_DIR  - where to find lpkit.h
+#  LPSolve_LIBRARIES    - list of libraries when using LPSolve
+#  LPSolve_DLLS         - list of DLLs when using LPSolve
+
+if(NOT LPSolve_INCLUDE_DIR)
+  set(__TRIAL_PATHS "$ENV{LPSOLVE_ROOT}/include" "${LPSOLVE_ROOT}" "${LPSOLVE_ROOT}/include"
+      /usr/include /usr/local/include /opt/include /usr/local/lpsolve
+      "$ENV{HOMEDRIVE}/Program Files/lpsolve/include"
+     )
+  find_path(LPSolve_INCLUDE_DIR lpkit.h ${__TRIAL_PATHS})
+  set(__TRIAL_PATHS "$ENV{LPSOLVE_ROOT}/lib" "${LPSOLVE_ROOT}" "${LPSOLVE_ROOT}/lib"
+      /usr/lib /usr/local/lib /opt/lib /usr/local/lpsolve
+      "$ENV{HOMEDRIVE}/Program Files/lpsolve/lib"
+     )
+  find_library(LPSolve_LIBRARIES lpsolve ${__TRIAL_PATHS})
+  if(NOT LPSolve_LIBRARIES)
+    find_library(LPSolve_LIBRARIES lpk5 ${__TRIAL_PATHS})
+  endif(NOT LPSolve_LIBRARIES)
+  if(NOT LPSolve_LIBRARIES)
+    find_library(LPSolve_LIBRARIES lpk ${__TRIAL_PATHS})
+  endif(NOT LPSolve_LIBRARIES)
+  set(__TRIAL_PATHS "$ENV{LPSOLVE_ROOT}/bin" "${LPSOLVE_ROOT}" "${LPSOLVE_ROOT}/bin"
+      "$ENV{HOMEDRIVE}/Program Files/lpsolve/bin"
+     )
+  find_path(LPSolve_DLL_DIR lpsolve.dll ${__TRIAL_PATHS})
+  if(LPSolve_DLL_DIR)
+    set(LPSolve_DLLS "${LPSolve_DLL_DIR}/lpsolve.dll" CACHE PATH "LPSolve DLL")
+  endif(LPSolve_DLL_DIR)
+
+  if(LPSolve_INCLUDE_DIR AND LPSolve_LIBRARIES)
+    set(LPSolve_FOUND 1 CACHE BOOL "Found LPSolve library")
+    message(STATUS "Found LPSolve Library: ${LPSolve_LIBRARIES}")
+  else(LPSolve_INCLUDE_DIR AND LPSolve_LIBRARIES)
+    set(LPSolve_FOUND 0 CACHE BOOL "Not found LPSolve library")
+    message(STATUS "LPSolve Library: not found")
+  endif(LPSolve_INCLUDE_DIR AND LPSolve_LIBRARIES)
+endif(NOT LPSolve_INCLUDE_DIR)
+
+mark_as_advanced(LPSolve_INCLUDE_DIR LPSolve_LIBRARIES LPSolve_DLL_DIR LPSolve_DLLS)
