6d241e25b7fd8a4250ae530a13cec0de8793ff3b
wistaria <wistaria@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2009-11-26 01:21:55 +0000

    Added install scripts for hdf5, cmake, lpsolve for ticket #55
    git-svn-id: svn://localhost:7744/trunk/alps@3395 3665984a-f8b0-4bb5-b30c-cdcaecbe7ced

---
+++ script/lpsolve.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+VERSION=4.0
+
+PREFIX="$1"
+BUILD_DIR="$2"
+SRC_DIR="$3"
+
+if test -z "$BUILD_DIR"; then
+  echo "$0 prefix build_dir [src_dir]"
+  exit 255
+fi
+
+LOG="$0.log.$$"
+echo "executing $0 $*" | tee "$LOG"
+
+URL="http://alps.comp-phys.org/static/software/lp_solve_$VERSION.tar.gz"
+SRC="$SRC_DIR/lp_solve_$VERSION.tar.gz"
+
+echo "cleaning up..." | tee -a "$LOG"
+if test -d "$BUILD_DIR"; then
+  rm -rf "$BUILD_DIR/lp_solve_$VERSION"
+else
+  mkdir -p "$BUILD_DIR"
+fi
+
+echo "retrieving source files..." | tee -a "$LOG"
+if test -n "$SRC_DIR" && test -f "$SRC"; then
+  (cd "$BUILD_DIR" && tar zxf $SRC) 2>&1 | tee -a "$LOG"
+else
+  CURL=`which curl`
+  if test -z "$CURL"; then
+    echo "curl utility not found" | tee -a "$LOG"
+    exit 255
+  fi
+  (cd "$BUILD_DIR" && "$CURL" "$URL" | tar zxf -) 2>&1 | tee -a "$LOG"
+fi
+
+( \
+echo "building..." && \
+(cd "$BUILD_DIR/lp_solve_$VERSION" && make) && \
+echo "installing..." && \
+mkdir -p "$prefix/include" "$prefix/lib" && \
+(cd "$BUILD_DIR/lp_solve_$VERSION" && cp -fp lpkit.h hash.h fortify.h declare.h "$prefix/include" && cp -fp liblpk.a "$prefix/lib") && \
+echo "cleaning up..." && \
+rm -rf "$BUILD_DIR/lp_solve_$VERSION" && \
+echo "done" \
+) 2>&1 | tee -a "$LOG"
+
+echo "log file = $LOG" | tee -a "$LOG"
