6fa87f67026dab65e2fc825f501d2230c84d193d
bela <bela@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2009-12-03 13:03:33 +0000

    Minor fix in DataSet class.
    git-svn-id: svn://localhost:7744/trunk/alps@3479 3665984a-f8b0-4bb5-b30c-cdcaecbe7ced

--- lib/pyalps/util/dataset.py
+++ lib/pyalps/util/dataset.py
@@ -16,8 +16,6 @@ class DataSet(ResultProperties):
         ret.props = copy.deepcopy(self.props,memo)
         ret.x = copy.deepcopy(self.x,memo)
         ret.y = copy.deepcopy(self.y,memo)
-        ret.dx = copy.deepcopy(self.dx,memo)
-        ret.dy = copy.deepcopy(self.dy,memo)
         return ret
 
     def __init__(self):
