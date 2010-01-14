8305e488cc0b30b2ce1ebda8586e730a3f0f5e0e
troyer <troyer@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2010-01-14 21:45:30 +0000

    Fixed write_xml_body again and optimized building of dmft by introducing a static library for shared code between the two applications
    git-svn-id: svn://localhost:7744/trunk/alps@3704 3665984a-f8b0-4bb5-b30c-cdcaecbe7ced

--- tool/maxent.hpp
+++ tool/maxent.hpp
@@ -95,7 +95,7 @@ public:
   
   MaxEntSimulation(const alps::ProcessList&, const boost::filesystem::path&);
   ~MaxEntSimulation();
-  void write_xml_body(alps::oxstream&, const boost::filesystem::path&, alps::hdf5::oarchive&) const;
+  void write_xml_body(alps::oxstream&, const boost::filesystem::path&) const;
   void dostep();
   vector_type levenberg_marquardt(vector_type u, const double alpha) const;
   vector_type iteration(vector_type u, const double alpha, const double mu) const;
