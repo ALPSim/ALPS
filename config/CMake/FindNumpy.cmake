0796807d74ee12464a941ca62c564df740d5a098
troyer <troyer@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2010-02-04 14:40:23 +0000

    Should also find numpy when building ALSp for Vistrails under Windows now
    git-svn-id: svn://localhost:7744/trunk/alps@3859 3665984a-f8b0-4bb5-b30c-cdcaecbe7ced

--- config/CMake/FindNumpy.cmake
+++ config/CMake/FindNumpy.cmake
@@ -7,7 +7,9 @@ find_package(PythonLibs REQUIRED)
 if (PYTHONLIBS_FOUND AND NOT PYTHON_NUMPY_INCLUDE_DIR)
     set(SEARCH_DIRS ${PYTHON_INCLUDE_DIR}/../Versions/Current/Extras/lib/python/numpy/core/include)
     if(VISTRAILS_APP_NAME)
-        set(SEARCH_DIRS ${SEARCH_DIRS} ${CMAKE_INSTALL_PREFIX}/${VISTRAILS_APP_NAME}/Contents/Resources/lib/python2.5/numpy/core/include)
+        set(SEARCH_DIRS ${SEARCH_DIRS} 
+           ${CMAKE_INSTALL_PREFIX}/${VISTRAILS_APP_NAME}/${VISTRAILS_PYTHON_EXTENSION_DIR}/numpy/core/include
+        )
     endif(VISTRAILS_APP_NAME)
     find_path(NUMPY_INCLUDE_DIR numpy/ndarrayobject.h ${SEARCH_DIRS})
             
