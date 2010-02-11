6e5fad7bfdcd23aa1e8c314464d836fbdbf751be
bela <bela@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2010-02-11 15:19:50 +0000

    Removed unnecessary dependency on scipy.
    git-svn-id: svn://localhost:7744/trunk/alps@3888 3665984a-f8b0-4bb5-b30c-cdcaecbe7ced

--- lib/pyalps/__init__.py
+++ lib/pyalps/__init__.py
@@ -26,7 +26,6 @@
 # ****************************************************************************
 
 from dataset import *
-from fit_wrapper import *
 from plot_core import *
 from floatwitherror import FloatWithError
 from loadhdf5_core import *
--- lib/pyalps/fit_wrapper.py
+++ lib/pyalps/fit_wrapper.py
@@ -25,7 +25,6 @@
 # 
 # ****************************************************************************
 
-import matplotlib.pyplot as plt
 import numpy as np
 from scipy import optimize
 
