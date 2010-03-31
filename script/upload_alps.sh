8df6d1d69ef4039bd133f7cbe2f272e4c5ff95ed
troyer <troyer@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2010-03-31 09:48:54 +0000

    Updated upload script
    git-svn-id: svn://localhost:7744/trunk/alps@4069 3665984a-f8b0-4bb5-b30c-cdcaecbe7ced

--- script/upload_alps.sh
+++ script/upload_alps.sh
@@ -5,7 +5,7 @@
 #          http://www.boost.org/LICENSE_1_0.txt)
 
 
-scp alps-vistrails*.* root@alps.ethz.ch:/var/www/vistrails
-scp alps-download*.* root@alps.ethz.ch:/var/www/software/releases
-scp alps-[12]*.* root@alps.ethz.ch:/var/www/software/releases
+scp alps-vistrails*.*[gez] root@alps.ethz.ch:/var/www/vistrails
+scp alps-download*.*[gez] root@alps.ethz.ch:/var/www/software/releases
+scp alps-2*.*[gez] root@alps.ethz.ch:/var/www/software/releases
 scp CPackUploads/*pkg CPackUploads/*zip root@alps.ethz.ch:/var/www/software/releases/packages
