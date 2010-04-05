a5cc389755e4a66395f534d657be8e4f955506e0
gullc <gullc@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2010-04-05 13:30:23 +0000

    mkstemp instead of mktemp
    git-svn-id: svn://localhost:7744/trunk/alps@4122 3665984a-f8b0-4bb5-b30c-cdcaecbe7ced

--- applications/dmft/qmc/externalsolver.C
+++ applications/dmft/qmc/externalsolver.C
@@ -36,8 +36,8 @@ ImpuritySolver::result_type ExternalSolver::solve(
 {
   char basename_in[]="alps_external_solver_XXXXXX";
   char basename_out[]="alps_external_solver_out_XXXXXX";
-  mktemp(basename_in); // the input file name
-  mktemp(basename_out); // the output file name
+  mkstemp(basename_in); // the input file name
+  mkstemp(basename_out); // the output file name
   std::string infile(basename_in), outfile(basename_out);
   if(parms.defined("TMPNAME")){
     infile=basename_in+std::string(".in.xml");
@@ -96,8 +96,8 @@ MatsubaraImpuritySolver::result_type ExternalSolver::solve_omega(
 {
   char basename_in[]="alps_external_solver_XXXXXX";
   char basename_out[]="alps_external_solver_out_XXXXXX";
-  mktemp(basename_in); // the input file name
-  mktemp(basename_out); // the output file name
+  mkstemp(basename_in); // the input file name
+  mkstemp(basename_out); // the output file name
   std::string infile(basename_in), outfile(basename_out);
   if(parms.defined("TMPNAME")){
     infile=basename_in+std::string(".in.xml");
