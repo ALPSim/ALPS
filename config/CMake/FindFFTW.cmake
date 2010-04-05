ae2a74f4e1afb56496e92fc5639f4f7b4b15b487
parcollet <parcollet@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2010-04-05 21:18:28 +0000

    Added FFTW detection  + exportation in ALPSConfig.cmake
    git-svn-id: svn://localhost:7744/trunk/alps@4130 3665984a-f8b0-4bb5-b30c-cdcaecbe7ced

--- config/CMake/FindFFTW.cmake
+++ config/CMake/FindFFTW.cmake
@@ -1,45 +1,34 @@
+#  Copyright Olivier Parcollet 2010.
+#  Distributed under the Boost Software License, Version 1.0.
+#      (See accompanying file LICENSE_1_0.txt or copy at
+#          http://www.boost.org/LICENSE_1_0.txt)
+
 #
-# this module look for boost (http://www.boost.org) support
-# it will define the following values
-#
-# BOOST_INCLUDE_DIR = where boost/boost.h can be found
-#
-# May want to define this but seldom required
-# BOOST_LIBRARY = where boost library can be found (reserved)
-#
+# This module looks for fftw.
+# It sets up : FFTW_INCLUDE_DIR, FFTW_LIBRARIES
+# 
 
-IF(FFTW_INCLUDE_DIRS)
-  FIND_PATH(FFTW_INCLUDE_DIR fftw3.h  ${FFTW_INCLUDE_DIRS})
-  FIND_LIBRARY(FFTW_LIBRARY fftw3 ${FFTW_LIBRARY_DIRS})
-ELSE(FFTW_INCLUDE_DIRS)
-  SET(TRIAL_PATHS
-    $ENV{FFTW_HOME}
-    /usr/include
-    /usr/local/include
-    /opt/include
-    /usr/apps/include
-  )
-  FIND_PATH(FFTW_INCLUDE_DIR fftw3.h ${TRIAL_PATHS})
+SET(TRIAL_PATHS
+ /usr/include
+ /usr/local/include
+ /opt/local/include
+ /sw/include
+ )
+FIND_PATH(FFTW_INCLUDE_DIR fftw3.h ${TRIAL_PATHS} DOC "Include for FFTW")
 
-  SET(TRIAL_LIBRARY_PATHS
-    $ENV{FFTW_HOME}/lib
-    /usr/lib 
-    /usr/local/lib
-    /opt/lib
-    /sw/lib
-    )
+SET(TRIAL_LIBRARY_PATHS
+ /usr/lib 
+ /usr/local/lib
+ /opt/local/lib
+ /sw/lib
+ )
 
-  FIND_LIBRARY(FFTW_LIBRARY fftw3 ${TRIAL_LIBRARY_PATHS})
+SET(FFTW_LIBRARIES "FFTW_LIBRARIES-NOTFOUND" CACHE STRING "FFTW library")
+# Try to detect the lib
+FIND_LIBRARY(FFTW_LIBRARIES fftw3 ${TRIAL_LIBRARY_PATHS} DOC "FFTW library")
 
-ENDIF(FFTW_INCLUDE_DIRS)
+mark_as_advanced(FFTW_INCLUDE_DIR)
+mark_as_advanced(FFTW_LIBRARIES)
 
-IF(FFTW_INCLUDE_DIR)
-  SET(FOUND_FFTW 1 CACHE BOOL "Found fftw library")
-ELSE(FFTW_INCLUDE_DIR)
-  SET(FOUND_FFTW 0 CACHE BOOL "Not Found fftw library")
-ENDIF(FFTW_INCLUDE_DIR)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(FFTW DEFAULT_MSG FFTW_LIBRARIES FFTW_INCLUDE_DIR)
 
-MARK_AS_ADVANCED(
-   FFTW_INCLUDE_DIR
-   FOUND_FFTW
-)
