60fad3cb6a977e8150789795582d7c6f147abeb6
parcollet <parcollet@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2010-04-06 19:33:16 +0000

    Added PYTHON_EXTRA_LIBS in  python linking. Slight addition to paths search in DocBook
    M    CMakeLists.txt
    M    config/CMake/FindDocbookDtd.cmake
    
    
    git-svn-id: svn://localhost:7744/trunk/alps@4138 3665984a-f8b0-4bb5-b30c-cdcaecbe7ced

--- config/CMake/FindDocbookDtd.cmake
+++ config/CMake/FindDocbookDtd.cmake
@@ -6,10 +6,13 @@
 # This module looks for fop and will define 
 # DOCBOOK_DTD_FOUND and DOCBOOK_DTD_DIR 
 
+# For fedora boxes
+file(GLOB P1 FOLLOW_SYMLINKS /usr/share/sgml/docbook/xml-dtd-4.2* )
+
 FIND_PATH(DOCBOOK_DTD_DIR
   NAMES docbookx.dtd
   PATHS /opt/local/share/xml/docbook/ ${Boost_ROOT_DIR}/tools/boostbook /usr/share/xml/docbook/schema/dtd/4.2/
-  /usr/share/sgml/docbook/xml-dtd-4.2-1.0-30.1/ 
+  ${P1}
   PATH_SUFFIXES 4.2 docbook-dtd-4.2
 )
 
