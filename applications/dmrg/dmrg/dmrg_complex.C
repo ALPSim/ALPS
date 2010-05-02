7cd7206ee94691709108536e14562768b1b288da
troyer <troyer@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2010-05-02 20:26:32 +0000

    Small bug fix in complex DMRG
    git-svn-id: svn://localhost:7744/trunk/alps@4301 3665984a-f8b0-4bb5-b30c-cdcaecbe7ced

--- applications/dmrg/dmrg/dmrg_complex.C
+++ applications/dmrg/dmrg/dmrg_complex.C
@@ -398,7 +398,7 @@ void DMRGTask::serialize(alps::hdf5::oarchive & ar) const
   for (std::size_t i=0;i<iteration_measurements.size();++i) {
     ar << alps::make_pvp("spectrum/iteration/"+boost::lexical_cast<std::string>(i),iteration_measurements[i]);
     std::map<std::string,std::vector<value_type> >::const_iterator it = iteration_measurements[i].average_values.find("Energy");
-    if (it != average_values.end())
+    if (it != iteration_measurements[i].average_values.end())
       ar << alps::make_pvp("spectrum/iteration/"+boost::lexical_cast<std::string>(i)+"/energies",it->second);
   }
 }
