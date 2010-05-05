52e9d8d564594792a0b53edc9a0975d983ece70f
troyer <troyer@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2010-05-05 18:32:57 +0000

    Fixed ticket #300. DO not build alpspython what building binary packages and fixed building of Boost.Test libraries
    git-svn-id: svn://localhost:7744/trunk/alps@4342 3665984a-f8b0-4bb5-b30c-cdcaecbe7ced

--- tutorials/mc-02-susceptibilities/tutorial2b.py
+++ tutorials/mc-02-susceptibilities/tutorial2b.py
@@ -9,7 +9,8 @@ for t in [0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.25, 1.5, 1.7
         { 
           'LATTICE'        : "ladder", 
           'T'              : t,
-          'J'              : -1 ,
+          'J0'             : -1 ,
+          'J1'             : -1 ,
           'THERMALIZATION' : 10000,
           'SWEEPS'         : 500000,
           'UPDATE'         : "cluster",
--- tutorials/mc-02-susceptibilities/tutorial2d.py
+++ tutorials/mc-02-susceptibilities/tutorial2d.py
@@ -13,8 +13,8 @@ for t in [0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.25, 1.5, 1.7
           'T'              : t,
           'J0'             : 1 ,
           'J1'             : 1 ,
-          'THERMALIZATION' : 10000,
-          'SWEEPS'         : 150000,
+          'THERMALIZATION' : 5000,
+          'SWEEPS'         : 50000,
           'L'              : 60,
           'ALGORITHM'      : "loop"
         }
