9204a1990367f40e26b050c137d7540a69c699ad
gukel <gukel@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2010-05-07 08:41:35 +0000

    Start work on ED-3 tutorials: Add spectra for spin chain and ladder.
    git-svn-id: svn://localhost:7744/trunk/alps@4385 3665984a-f8b0-4bb5-b30c-cdcaecbe7ced

---
+++ tutorials/07-sparsediag/tutorial7f.py
@@ -0,0 +1,51 @@
+import pyalps
+import numpy as np
+import matplotlib.pyplot as plt
+import pyalps.pyplot
+
+#prepare the input parameters
+parms=[]
+for l in [10, 12, 14, 16]:
+    parms.append(
+      { 
+        'LATTICE'                   : "chain lattice", 
+        'MODEL'                     : "spin",
+        'local_S'                   : 0.5,
+        'J'                         : 1,
+        'L'                         : l,
+        'CONSERVED_QUANTUMNUMBERS'  : 'Sz',
+        'Sz_total'                  : 0
+      }
+    )
+
+#write the input file and run the simulation
+input_file = pyalps.writeInputFiles('parm7f',parms)
+res = pyalps.runApplication('sparsediag',input_file)
+
+#load all measurements for all states
+data = pyalps.loadSpectra(pyalps.getResultFiles(prefix='parm7f'))
+
+# collect spectra over all momenta for every simulation
+spectra = {}
+for sim in data:
+  l = int(sim[0].props['L'])
+  all_energies = []
+  spectrum = pyalps.DataSet()
+  for sec in sim:
+    all_energies += list(sec.y)
+    spectrum.x = np.concatenate((spectrum.x,np.array([sec.props['TOTAL_MOMENTUM'] for i in range(len(sec.y))])))
+    spectrum.y = np.concatenate((spectrum.y,sec.y))
+  spectrum.y -= np.min(all_energies)
+  spectrum.props['line'] = 'scatter'
+  spectrum.props['label'] = 'L='+str(l)
+  spectra[l] = spectrum
+
+
+# plot
+plt.figure()
+pyalps.pyplot.plot(spectra.values())
+plt.legend()
+plt.xlim(0,2*3.1416)
+plt.ylabel('Energy')
+plt.ylim(0,2)
+plt.show()
---
+++ tutorials/07-sparsediag/tutorial7g.py
@@ -0,0 +1,55 @@
+import pyalps
+import numpy as np
+import matplotlib.pyplot as plt
+import pyalps.pyplot
+
+#prepare the input parameters
+parms=[]
+for l in [4, 6, 8]:
+    parms.append(
+      { 
+        'LATTICE'                   : "ladder", 
+        'MODEL'                     : "spin",
+        'local_S'                   : 0.5,
+        'J0'                        : 1,
+        'J1'                        : 1,
+        'L'                         : l,
+        'CONSERVED_QUANTUMNUMBERS'  : 'Sz',
+        'Sz_total'                  : 0
+      }
+    )
+
+#write the input file and run the simulation
+input_file = pyalps.writeInputFiles('parm7g',parms)
+res = pyalps.runApplication('sparsediag',input_file)
+
+#load all measurements for all states
+data = pyalps.loadSpectra(pyalps.getResultFiles(prefix='parm7g'))
+
+groundstates = {} # {L: E_0}
+spectra = {}    # {(L,sz): ([k],[E])}
+
+# collect spectra over all momenta for every simulation
+spectra = {}
+for sim in data:
+  l = int(sim[0].props['L'])
+  all_energies = []
+  spectrum = pyalps.DataSet()
+  for sec in sim:
+    all_energies += list(sec.y)
+    spectrum.x = np.concatenate((spectrum.x,np.array([sec.props['TOTAL_MOMENTUM'] for i in range(len(sec.y))])))
+    spectrum.y = np.concatenate((spectrum.y,sec.y))
+  spectrum.y -= np.min(all_energies)
+  spectrum.props['line'] = 'scatter'
+  spectrum.props['label'] = 'L='+str(l)
+  spectra[l] = spectrum
+
+
+# plot
+plt.figure()
+pyalps.pyplot.plot(spectra.values())
+plt.legend()
+plt.xlim(0,2*3.1416)
+plt.ylabel('Energy')
+plt.ylim(0,2.5)
+plt.show()
