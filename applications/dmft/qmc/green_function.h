7f41bb4d2566e43030de14595d8e65164efefa1d
gullc <gullc@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2010-05-10 19:48:51 +0000

    no error writing for Green's functions.
    git-svn-id: svn://localhost:7744/trunk/alps@4448 3665984a-f8b0-4bb5-b30c-cdcaecbe7ced

--- applications/dmft/qmc/green_function.h
+++ applications/dmft/qmc/green_function.h
@@ -124,9 +124,10 @@ template <typename T> class green_function{
         for(unsigned int j=0;j<ns_;++j){
           for(unsigned int k=0;k<ns_;++k){
             std::stringstream subpath; subpath<<path<<"/"<<i<<"/"<<j<<"/"<<"/"<<k<<"/values/mean";
-            std::stringstream subpath_e; subpath_e<<path<<"/"<<i<<"/"<<j<<"/"<<"/"<<k<<"/values/error";
+            //currently we're not writing the error.
+            //std::stringstream subpath_e; subpath_e<<path<<"/"<<i<<"/"<<j<<"/"<<"/"<<k<<"/values/error";
             ar<<alps::make_pvp(subpath.str(), val_, nt_);
-            ar<<alps::make_pvp(subpath_e.str(), err_, nt_);
+            //ar<<alps::make_pvp(subpath_e.str(), err_, nt_);
           }
         }
       }
