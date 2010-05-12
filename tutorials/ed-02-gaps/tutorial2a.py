6a2f0f3e3ec8bc5de2842c608d91a8adc1bb6b77
bela <bela@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2010-05-12 07:32:03 +0000

    Resolved #309 for Python
    git-svn-id: svn://localhost:7744/trunk/alps@4463 3665984a-f8b0-4bb5-b30c-cdcaecbe7ced

--- tutorials/ed-02-gaps/tutorial2a.py
+++ tutorials/ed-02-gaps/tutorial2a.py
@@ -2,10 +2,11 @@ import pyalps
 import numpy as np
 import matplotlib.pyplot as plt
 import pyalps.pyplot
+import pyalps.fit_wrapper as fw
 
 #prepare the input parameters
 parms = []
-for l in [4, 6, 8, 10]:
+for l in [4, 6, 8, 10, 12, 14]:
   for sz in [0, 1]:
       parms.append(
         { 
@@ -21,7 +22,7 @@ for l in [4, 6, 8, 10]:
 
 #write the input file and run the simulation
 input_file = pyalps.writeInputFiles('parm2a',parms)
-res = pyalps.runApplication('sparsediag',input_file)
+res = pyalps.runApplication('sparsediag',input_file) # ,MPI=4)
 
 #load all measurements for all states
 data = pyalps.loadSpectra(pyalps.getResultFiles(prefix='parm2a'))
@@ -47,10 +48,24 @@ gapplot.y = [min_energies[(l,1)] -min_energies[(l,0)] for l in np.sort(lengths)]
 gapplot.props['xlabel']='$1/L$'
 gapplot.props['ylabel']='Triplet gap $\Delta/J$'
 gapplot.props['label']='S=1'
+gapplot.props['line']='.'
 
 plt.figure()
 pyalps.pyplot.plot(gapplot)
 plt.legend()
 plt.xlim(0,0.25)
 plt.ylim(0,1.0)
+
+d0 = fw.Parameter(0.411)
+L0 = fw.Parameter(1000)
+a = fw.Parameter(1)
+f = lambda self, x: d0()+a()*np.exp(-x/L0())
+# we fit only a range from 8 to 14
+fw.fit(None, f, (d0,L0,a), np.array(gapplot.y)[2:], np.sort(lengths)[2:])
+
+x = np.linspace(0, 1./min(lengths), 100)
+plt.plot(x, f(None, 1/x))
+
 plt.show()
+
+
