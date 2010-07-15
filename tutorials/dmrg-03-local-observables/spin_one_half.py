f6158e70785b6e5aed76942acb9f8ef70b5ba38e
afeiguin <afeiguin@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2010-07-15 23:29:33 +0000

    use L=32
    git-svn-id: svn://localhost:7744/trunk/alps@4742 3665984a-f8b0-4bb5-b30c-cdcaecbe7ced

--- tutorials/dmrg-03-local-observables/spin_one_half.py
+++ tutorials/dmrg-03-local-observables/spin_one_half.py
@@ -41,7 +41,7 @@ for sz in [0,1,2]:
         'J'                                     : 1,
         'SWEEPS'                                : 4,
         'NUMBER_EIGENVALUES'                    : 1,
-        'L'                                     : 96,
+        'L'                                     : 32,
         'MAXSTATES'                             : 40,
         'MEASURE_LOCAL[Local magnetization]'    : 'Sz'
        } )
