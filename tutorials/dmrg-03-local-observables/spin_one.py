be42a0c5f18c7c548234ce96588cf041b262b794
gukel <gukel@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2010-07-19 20:12:30 +0000

    dmrg-03 tutorial: convert tabs to spaces
    git-svn-id: svn://localhost:7744/trunk/alps@4754 3665984a-f8b0-4bb5-b30c-cdcaecbe7ced

--- tutorials/dmrg-03-local-observables/spin_one.py
+++ tutorials/dmrg-03-local-observables/spin_one.py
@@ -33,20 +33,20 @@ import pyalps.pyplot
 #prepare the input parameters
 parms = []
 for sz in [0,1,2]:
-		parms.append( { 
-	'LATTICE_LIBRARY'           : 'my_lattices.xml',
-	'LATTICE'                   : 'open chain lattice with special edges 32',
-	'MODEL'                     : "spin",
-	'local_S0'                  : '0.5',
-	'local_S1'                  : '1',
-	'CONSERVED_QUANTUMNUMBERS'  : 'N,Sz',
-	'Sz_total'                  : sz,
-	'J'                         : 1,
-	'SWEEPS'                    : 4,
-	'NUMBER_EIGENVALUES'        : 1,
-	'MAXSTATES'                 : 40,
-    'MEASURE_LOCAL[Local magnetization]'   : 'Sz'
-   } )
+    parms.append( { 
+        'LATTICE_LIBRARY'           : 'my_lattices.xml',
+        'LATTICE'                   : 'open chain lattice with special edges 32',
+        'MODEL'                     : "spin",
+        'local_S0'                  : '0.5',
+        'local_S1'                  : '1',
+        'CONSERVED_QUANTUMNUMBERS'  : 'N,Sz',
+        'Sz_total'                  : sz,
+        'J'                         : 1,
+        'SWEEPS'                    : 4,
+        'NUMBER_EIGENVALUES'        : 1,
+        'MAXSTATES'                 : 40,
+        'MEASURE_LOCAL[Local magnetization]'   : 'Sz'
+    } )
 
 #write the input file and run the simulation
 input_file = pyalps.writeInputFiles('parm_spin_one',parms)
