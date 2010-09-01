fa258171c04bba8b91b735bfbc521f6be0af6e7b
gamperl <gamperl@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2010-09-01 13:40:09 +0000

    fixed typo
    git-svn-id: svn://localhost:7744/trunk/alps@4824 3665984a-f8b0-4bb5-b30c-cdcaecbe7ced

--- config/run_python_test.cmake
+++ config/run_python_test.cmake
@@ -3,7 +3,7 @@
 #      (See accompanying file LICENSE_1_0.txt or copy at
 #          http://www.boost.org/LICENSE_1_0.txt)
 
-file(WRITE tmp_${cmd}.sh "PYTHONPATH=\$PYTHONPATH:{binarydir}/lib python ${sourcedir}/${cmd}")
+file(WRITE tmp_${cmd}.sh "PYTHONPATH=\$PYTHONPATH:${binarydir}/lib python ${sourcedir}/${cmd}")
 
 execute_process(
     COMMAND sh tmp_${cmd}.sh
