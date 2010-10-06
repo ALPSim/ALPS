769bba2e18c25e034c81dbad29e229243094fc2b
halm <halm@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2010-10-06 10:34:37 +0000

    Patch for Fujitsu C++ compiler (FCC)
    git-svn-id: svn://localhost:7744/trunk/alps@4913 3665984a-f8b0-4bb5-b30c-cdcaecbe7ced

--- src/boost/numeric/bindings/detail/config/fortran.hpp
+++ src/boost/numeric/bindings/detail/config/fortran.hpp
@@ -16,7 +16,7 @@
 
 // First we need to know what the conventions for linking
 // C with Fortran is on this platform/toolset
-#if defined(__GNUC__) || defined(__ICC) || defined(__sgi) || defined(__COMO__) || defined(__KCC)
+#if defined(__GNUC__) || defined(__ICC) || defined(__sgi) || defined(__COMO__) || defined(__KCC) || defined(__FCC_VERSION)
 #define BIND_FORTRAN_LOWERCASE_UNDERSCORE
 #elif defined(__IBMCPP__) || defined(_MSC_VER)
 #define BIND_FORTRAN_LOWERCASE
