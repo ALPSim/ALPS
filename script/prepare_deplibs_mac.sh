7ccfdbb27851bbc53cf5191ce77e09bf0206252e
troyer <troyer@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2010-10-12 05:43:36 +0000

    Worked on Fortran integration
    git-svn-id: svn://localhost:7744/trunk/alps@4923 3665984a-f8b0-4bb5-b30c-cdcaecbe7ced

--- script/prepare_deplibs_mac.sh
+++ script/prepare_deplibs_mac.sh
@@ -6,32 +6,28 @@
 
 mkdir -p deplibs/lib
 mkdir -p deplibs/bin
-cp /opt/local/lib/libhdf5*dylib deplibs/lib
+cp /opt/alps/lib/libhdf5*dylib deplibs/lib
 cp /opt/alps/lib/libhdf5*a deplibs/lib
 cp /opt/local/lib/libsz*dylib deplibs/lib
 cp /opt/local/lib/libz*dylib deplibs/lib
-install_name_tool -id /opt/alps/lib/libhdf5.6.dylib deplibs/lib/libhdf5.dylib
-install_name_tool -id /opt/alps/lib/libhdf5.6.dylib deplibs/lib/libhdf5.6.dylib
-install_name_tool -id /opt/alps/lib/libhdf5_hl.6.dylib deplibs/lib/libhdf5_hl.dylib
-install_name_tool -id /opt/alps/lib/libhdf5_hl.6.dylib deplibs/lib/libhdf5_hl.6.dylib
 install_name_tool -id /opt/alps/lib/libsz.2.dylib deplibs/lib/libsz.dylib
 install_name_tool -id /opt/alps/lib/libsz.2.dylib deplibs/lib/libsz.2.dylib
 install_name_tool -id /opt/alps/lib/libsz.2.dylib deplibs/lib/libsz.2.0.0.dylib
 install_name_tool -id /opt/alps/lib/libz.1.dylib deplibs/lib/libz.dylib
 install_name_tool -id /opt/alps/lib/libz.1.dylib deplibs/lib/libz.1.dylib
 install_name_tool -id /opt/alps/lib/libz.1.dylib deplibs/lib/libz.1.2.*.dylib
-install_name_tool -change /opt/local/lib/libsz.2.dylib /opt/alps/lib/libsz.2.dylib deplibs/lib/libhdf5.dylib
-install_name_tool -change /opt/local/lib/libsz.2.dylib /opt/alps/lib/libsz.2.dylib deplibs/lib/libhdf5.*.dylib
-install_name_tool -change /opt/local/lib/libsz.2.dylib /opt/alps/lib/libsz.2.dylib deplibs/lib/libhdf5_hl.dylib
-install_name_tool -change /opt/local/lib/libsz.2.dylib /opt/alps/lib/libsz.2.dylib deplibs/lib/libhdf5_hl.6.dylib
-install_name_tool -change /opt/local/lib/libz.1.dylib /opt/alps/lib/libz.1.dylib deplibs/lib/libhdf5.dylib
-install_name_tool -change /opt/local/lib/libz.1.dylib /opt/alps/lib/libz.1.dylib deplibs/lib/libhdf5.6.dylib
-install_name_tool -change /opt/local/lib/libz.1.dylib /opt/alps/lib/libz.1.dylib deplibs/lib/libhdf5_hl.dylib
-install_name_tool -change /opt/local/lib/libz.1.dylib /opt/alps/lib/libz.1.dylib deplibs/lib/libhdf5_hl.6.dylib
+#install_name_tool -change /opt/local/lib/libsz.2.dylib /opt/alps/lib/libsz.2.dylib deplibs/lib/libhdf5.dylib
+#install_name_tool -change /opt/local/lib/libsz.2.dylib /opt/alps/lib/libsz.2.dylib deplibs/lib/libhdf5.*.dylib
+#install_name_tool -change /opt/local/lib/libsz.2.dylib /opt/alps/lib/libsz.2.dylib deplibs/lib/libhdf5_hl.dylib
+#install_name_tool -change /opt/local/lib/libsz.2.dylib /opt/alps/lib/libsz.2.dylib deplibs/lib/libhdf5_hl.6.dylib
+#install_name_tool -change /opt/local/lib/libz.1.dylib /opt/alps/lib/libz.1.dylib deplibs/lib/libhdf5.dylib
+#install_name_tool -change /opt/local/lib/libz.1.dylib /opt/alps/lib/libz.1.dylib deplibs/lib/libhdf5.6.dylib
+#install_name_tool -change /opt/local/lib/libz.1.dylib /opt/alps/lib/libz.1.dylib deplibs/lib/libhdf5_hl.dylib
+#install_name_tool -change /opt/local/lib/libz.1.dylib /opt/alps/lib/libz.1.dylib deplibs/lib/libhdf5_hl.6.dylib
 
 mkdir -p deplibs/include
-cp /opt/local/include/H5* deplibs/include
-cp /opt/local/include/hdf* deplibs/include
+cp /opt/alps/include/H5* deplibs/include
+cp /opt/alps/include/hdf* deplibs/include
 cp /opt/local/include/zconf.h deplibs/include
 cp /opt/local/include/zlib.h deplibs/include
 cp /opt/local/include/szlib.h deplibs/include
