161a041d4889c9d0ea468bf5dcd51459b9b46603
troyer <troyer@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2010-11-01 10:07:50 +0000

    Deprecated old bindings
    git-svn-id: svn://localhost:7744/trunk/alps@5008 3665984a-f8b0-4bb5-b30c-cdcaecbe7ced

---
+++ src/boost/numeric/bindings_new/ublas/matrix_expression.hpp
@@ -0,0 +1,70 @@
+//
+// Copyright (c) 2009 Rutger ter Borg
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#ifndef BOOST_NUMERIC_BINDINGS_UBLAS_MATRIX_EXPRESSION_HPP
+#define BOOST_NUMERIC_BINDINGS_UBLAS_MATRIX_EXPRESSION_HPP
+
+#include <boost/numeric/bindings/bandwidth.hpp>
+#include <boost/numeric/bindings/begin.hpp>
+#include <boost/numeric/bindings/detail/adaptor.hpp>
+#include <boost/numeric/bindings/detail/property_map.hpp>
+#include <boost/numeric/bindings/end.hpp>
+#include <boost/numeric/bindings/size.hpp>
+#include <boost/numeric/ublas/matrix_expression.hpp>
+
+namespace boost {
+namespace numeric {
+namespace bindings {
+namespace detail {
+
+template< typename T, typename Id, typename Enable >
+struct adaptor< boost::numeric::ublas::matrix_reference< T >, Id, Enable > {
+
+    typedef typename copy_const< Id, T >::type adapted_type;
+    typedef typename property_map_of< adapted_type >::type property_map;
+
+    static std::ptrdiff_t size1( const Id& id ) {
+        return id.size1();
+    }
+
+    static std::ptrdiff_t size2( const Id& id ) {
+        return id.size2();
+    }
+
+    static typename result_of::begin_value< adapted_type >::type begin_value( Id& id ) {
+        return bindings::begin_value( id.expression() );
+    }
+
+    static typename result_of::end_value< adapted_type >::type end_value( Id& id ) {
+        return bindings::end_value( id.expression() );
+    }
+
+    static std::ptrdiff_t stride1( const Id& id ) {
+        return bindings::stride1( id.expression() );
+    }
+
+    static std::ptrdiff_t stride2( const Id& id ) {
+        return bindings::stride2( id.expression() );
+    }
+
+    static std::ptrdiff_t bandwidth1( const Id& id ) {
+        return bindings::bandwidth1( id.expression() );
+    }
+
+    static std::ptrdiff_t bandwidth2( const Id& id ) {
+        return bindings::bandwidth2( id.expression() );
+    }
+
+};
+
+} // namespace detail
+} // namespace bindings
+} // namespace numeric
+} // namespace boost
+
+#endif
