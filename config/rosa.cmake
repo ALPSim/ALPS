a5b73586ea0c17d20bc204e923b5b063c8ed2820
gamperl <gamperl@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2010-11-24 12:31:32 +0000

    disabled phtyon tests on static builds added machine config file for rosa
    git-svn-id: svn://localhost:7744/trunk/alps@5197 3665984a-f8b0-4bb5-b30c-cdcaecbe7ced

---
+++ config/rosa.cmake
@@ -0,0 +1,14 @@
+#  Copyright Matthias Troyer, Synge Todo and Lukas Gamper 2009 - 2010.
+#  Distributed under the Boost Software License, Version 1.0.
+#      (See accompanying file LICENSE_1_0.txt or copy at
+#          http://www.boost.org/LICENSE_1_0.txt)
+
+SET(CMAKE_CXX_COMPILER CC)
+SET(CMAKE_C_COMPILER cc)
+SET(CMAKE_Fortran_COMPILER ftn)
+SET(ALPS_BUILD_PYTHON OFF)
+SET(BUILD_SHARED_LIBS OFF)
+SET(BLAS_LIBRARY "/opt/xt-libsci/default/gnu/lib/libsci.a")
+SET(LAPACK_LIBRARY "/opt/xt-libsci/default/gnu/lib/libsci.a")
+SET(MPI_INCLUDE_PATH "/opt/cray/mpt/5.1.1/xt/seastar/mpich2-gnu/include")
+SET(MPI_LIBRARY "/opt/cray/mpt/5.1.1/xt/seastar/mpich2-gnu/lib/libmpich.a")
