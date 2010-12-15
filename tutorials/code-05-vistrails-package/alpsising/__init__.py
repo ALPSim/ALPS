54f8065e0b32d4ad185bc2da0eba76684265db9e
troyer <troyer@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2010-12-15 10:54:26 +0000

    Simplified alpsising tutorial package to make it work with the atest VisTrails version
    git-svn-id: svn://localhost:7744/trunk/alps@5338 3665984a-f8b0-4bb5-b30c-cdcaecbe7ced

--- tutorials/code-05-vistrails-package/alpsising/__init__.py
+++ tutorials/code-05-vistrails-package/alpsising/__init__.py
@@ -9,20 +9,16 @@
 #
 ##############################################################################
 
-from core.modules.vistrails_module import Module
-import core.modules.basic_modules
-from packages.alps.applications import RunAlpsApplication
-import ising
-import os
-
-from packages.alps.parameters import Parameters 
-basic = core.modules.basic_modules
-
 identifier = 'org.comp-phys.alpsising'
 version = '2.0.0'
 name = 'Full ALPS Package tutorial'
 
 
 def package_dependencies():
-  return ['org.comp-phys.alps']
+    import core.packagemanager
+    manager = core.packagemanager.get_package_manager()
+    if manager.has_package('org.comp-phys.alps'):
+      return ['org.comp-phys.alps']
+    else:
+      return []
 
