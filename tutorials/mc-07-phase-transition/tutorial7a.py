b810d7afa400a443b4e6849b39b8d5783f464a22
bela <bela@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2011-01-11 15:33:23 +0000

    Changed MC07 workflow to get files from archive disk with HTTPFile
    git-svn-id: svn://localhost:7744/trunk/alps@5370 3665984a-f8b0-4bb5-b30c-cdcaecbe7ced

--- tutorials/mc-07-phase-transition/tutorial7a.py
+++ tutorials/mc-07-phase-transition/tutorial7a.py
@@ -61,7 +61,7 @@ for l in [4,8,16]:
 
 #write the input file and run the simulation
 input_file = pyalps.writeInputFiles('parm7a',parms)
-pyalps.runApplication('spinmc',input_file,Tmin=5)
+pyalps.runApplication('spinmc',input_file,Tmin=5,MPI=2)
 
 pyalps.evaluateSpinMC(pyalps.getResultFiles(prefix='parm7a'))
 
