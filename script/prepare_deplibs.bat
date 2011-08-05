0e6c4543ee581a8e2ca8b464d4be4d67d24fdc52
troyer <troyer@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2011-08-05 03:15:16 +0000

    Added use of Boost_INCLUDE_DIR and fixed preparation of dependent libraries on Windows
    git-svn-id: svn://localhost:7744/trunk/alps@5676 3665984a-f8b0-4bb5-b30c-cdcaecbe7ced

--- script/prepare_deplibs.bat
+++ script/prepare_deplibs.bat
@@ -1,5 +1,5 @@
 rem
-rem Copyright (C) 2009 - 2010 Matthias Troyer
+rem Copyright (C) 2009 - 2011 Matthias Troyer
 rem
 rem Distributed under the Boost Software License, Version 1.0. (See accompany-
 rem ing file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -8,6 +8,8 @@ mkdir deplibs
 mkdir deplibs\bin
 mkdir deplibs\lib
 mkdir deplibs\include
+mkdir deplibs\share
+mkdir deplibs\etc
 copy ..\..\opt\lib deplibs\lib
 copy ..\..\opt\bin deplibs\bin
 copy deplibs\bin\lpsolve.dll deplibs\bin\lpsolve51.dll
