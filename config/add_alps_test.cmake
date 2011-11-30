e61dcea89b9aa9c24d633dfcdc228f38695633ee
wistaria <wistaria@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2011-11-30 03:08:30 +0000

    Added run_test_mpi.cmake script file
    git-svn-id: svn://localhost:7744/trunk/alps@5851 3665984a-f8b0-4bb5-b30c-cdcaecbe7ced

--- config/add_alps_test.cmake
+++ config/add_alps_test.cmake
@@ -50,3 +50,71 @@ macro(add_alps_test)
       )
   endif(EXISTS ${PROJECT_SOURCE_DIR}/config/run_test.cmake)
 endmacro(add_alps_test)
+
+macro(add_alps_test_mpi)
+  if(${ARGC} EQUAL 1)
+    set(name ${ARGV0})
+    set(procs 1)
+    set(input ${ARGV0})
+    set(output ${ARGV0})
+  else(${ARGC} EQUAL 1)
+    if(${ARGC} EQUAL 2)
+      set(name ${ARGV0})
+      set(procs ${ARGV1})
+      set(input ${ARGV0})
+      set(output ${ARGV0})
+    else(${ARGC} EQUAL 2)
+      if(${ARGC} EQUAL 3)
+        set(name ${ARGV0})
+        set(procs ${ARGV1})
+        set(input ${ARGV2})
+        set(output ${ARGV2})
+      else(${ARGC} EQUAL 3)
+        set(name ${ARGV0})
+        set(procs ${ARGV1})
+        set(input ${ARGV2})
+        set(output ${ARGV3})
+      endif(${ARGC} EQUAL 3)
+    endif(${ARGC} EQUAL 2)
+  endif(${ARGC} EQUAL 1)
+  enable_testing()
+  if(MSVC)
+    get_target_property(EXE_NAME ${name} LOCATION)
+    add_custom_command(TARGET ${name} POST_BUILD
+      COMMAND ${CMAKE_COMMAND} -E copy ${EXE_NAME} ${PROJECT_BINARY_DIR}/bin)
+  endif(MSVC)
+  
+  if(EXISTS ${PROJECT_SOURCE_DIR}/config/run_test_mpi.cmake)
+    add_test(${name}-np${procs}
+      ${CMAKE_COMMAND}
+        -Dcmd=${name}
+        -Dmpiexec=${MPIEXEC}
+        -Dmpiexec_numproc_flag=${MPIEXEC_NUMPROC_FLAG}
+        -Dprocs=${procs}
+        -Dmpiexec_preflags=${MPIEXEC_PREFLAGS}
+        -Dmpiexec_postflags=${MPIEXEC_POSTFLAGS}
+        -Dsourcedir=${CMAKE_CURRENT_SOURCE_DIR}
+        -Dbinarydir=${CMAKE_CURRENT_BINARY_DIR}
+        -Ddllexedir=${PROJECT_BINARY_DIR}/bin
+        -Dinput=${input}
+        -Doutput=${output}
+        -P ${PROJECT_SOURCE_DIR}/config/run_test_mpi.cmake
+      )
+  else(EXISTS ${PROJECT_SOURCE_DIR}/config/run_test.cmake)
+    add_test(${name}-np${procs}
+      ${CMAKE_COMMAND}
+        -Dcmd=${name}
+        -Dmpiexec=${MPIEXEC}
+        -Dmpiexec_numproc_flag=${MPIEXEC_NUMPROC_FLAG}
+        -Dprocs=${procs}
+        -Dmpiexec_preflags=${MPIEXEC_PREFLAGS}
+        -Dmpiexec_postflags=${MPIEXEC_POSTFLAGS}
+        -Dsourcedir=${CMAKE_CURRENT_SOURCE_DIR}
+        -Dbinarydir=${CMAKE_CURRENT_BINARY_DIR}
+        -Ddllexedir=${PROJECT_BINARY_DIR}/bin
+        -Dinput=${input}
+        -Doutput=${output}
+        -P ${CMAKE_INSTALL_PREFIX}/share/alps/run_test_mpi.cmake
+      )
+  endif(EXISTS ${PROJECT_SOURCE_DIR}/config/run_test_mpi.cmake)
+endmacro(add_alps_test_mpi)
