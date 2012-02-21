1c8d7f2fda414f17947ce230dac58749cb0a9a0e
poprawem <poprawem@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2012-02-21 14:22:33 +0000

    added python documentation for mcanalyze functions and classes.
    git-svn-id: svn://localhost:7744/trunk/alps@6002 3665984a-f8b0-4bb5-b30c-cdcaecbe7ced

--- lib/pyalps/alea.py
+++ lib/pyalps/alea.py
@@ -31,45 +31,85 @@ This module contains classes for the evaluation of Monte Carlo measurements:
 - RealVectorObservable
 - RealTimeSeriesObservable
 - RealVectorTimeSeriesObservable
+
+- MCScalarTimeseries
+- MCVectorTimeseries
+- MCScalarTimeseriesView
+- MCVectorTimeseriesView
 """
 
-from pyalea_c import *
 from pymcdata_c import *
+from pyalea_c import RealObservable, RealVectorObservable, RealTimeSeriesObservable, RealVectorTimeSeriesObservable
+from pyalea_c import MCScalarTimeseries, MCScalarTimeseriesView, MCVectorTimeseries, MCVectorTimeseries, ValueWithError, StdPairDouble, size, mean, variance, integrated_autocorrelation_time, running_mean, reverse_running_mean
+import alea_detail as detail
 
-def autocorrelation(obs, _distance = None, _limit = None):
+
+#def std_pair_double.__repr__():
+#  return str(self.first) + ", " + str(self.second)
+
+
+def autocorrelation(timeseries, _distance = None, _limit = None):
+  "Calculates the autocorrelation of a given timeseries. Can be invoked with _distance or _limit.\n\
+timeseries: Any MCTimeseries or MCData object\n\
+_distance: Calculates the autocorrelation until a specific length\n\
+_limit: Calculates the autocorrelation until it has reached _limit of its initial value\n\
+returns: MCTimeseries object with the autocorrelation"
   if _distance != None:
-    return autocorrelation_distance(obs, _distance)
+    return detail.autocorrelation_distance(timeseries, _distance)
   if _limit != None:
-    return autocorrelation_limit(obs, _limit)
-  print "Usage: autocorrelation(obs, [_distance = XXX | _limit = XXX] )"
+    return detail.autocorrelation_limit(timeseries, _limit)
+  print "Usage: autocorrelation(timeseries, [_distance = XXX | _limit = XXX] )"
 
-def cut_head(obs, _distance = None, _limit = None):
+def cut_head(timeseries, _distance = None, _limit = None):
+  "Creates a MCTimeseriesView object. Can be invoked with _distance or _limit.\n\
+timeseries: Any MCTimeseries object\n\
+_distance: Cuts the first _distance entries\n\
+_limit: Cuts the front until the timeseries reaches _limit of its initial value\n\
+returns: MCTimeseriesView object with the smaller timeseries\n\
+  Note: does not copy the data, only creates a reference."
   if _distance != None:
-    return cut_head_distance(obs, _distance)
+    return detail.cut_head_distance(timeseries, _distance)
   if _limit != None:
-    return cut_head_limit(obs, _limit)
-  print "Usage: cut_head(obs, [_distance = XXX | _limit = XXX] )"
+    return detail.cut_head_limit(timeseries, _limit)
+  print "Usage: cut_head(timeseries, [_distance = XXX | _limit = XXX] )"
 
-def cut_tail(obs, _distance = None, _limit = None):
+def cut_tail(timeseries, _distance = None, _limit = None):
+  "Creates a MCTimeseriesView object. Can be invoked with _distance or _limit.\n\
+timeseries: Any MCTimeseries object\n\
+_distance: Cuts the last _distance entries\n\
+_limit: Cuts the tail until the timeseries only decays from its initial value to _limit of its initial value\n\
+returns: MCTimeseriesView object with the smaller timeseries\n\
+  Note: does not copy the data, only creates a reference."
   if _distance != None:
-    return cut_tail_distance(obs, _distance)
+    return detail.cut_tail_distance(timeseries, _distance)
   if _limit != None:
-    return cut_tail_limit(obs, _limit)
-  print "Usage: cut_head(obs, [_distance = XXX | _limit = XXX] )"
+    return detail.cut_tail_limit(timeseries, _limit)
+  print "Usage: cut_head(timeseries, [_distance = XXX | _limit = XXX] )"
 
-def exponential_autocorrelation_time(obs, _from = None, _to = None, _max = None, _min = None):
+def exponential_autocorrelation_time(autocorrelation, _from = None, _to = None, _max = None, _min = None):
+  "Fits a timeseries exponentially. Can be invoked with _from and _to or with _max and _min.\n\
+autocorrelation: A MCTimeseries object\n\
+_from & _to: fits the autocorrelation between _from and _to\n\
+_max & _min: fits the autocorrelation between the values where it is at _max and where it is at _min from its initial value\n\
+returns: StdPairDouble object FIT with the parameters of the fit\n\
+  Note: The equation is FIT.first * exp(FIT.second * t)"
   if (_from != None and _to != None):
-    return exponential_autocorrelation_time_distance(obs, _from, _to)
+    return detail.exponential_autocorrelation_time_distance(timeseries, _from, _to)
   if (_max != None and _min != None):
-    return exponential_autocorrelation_time_limit(obs, _max, _min)
-  print "Usage: exponential_autocorrelation_time(obs, [_from = XXX, _to = XXX | _max = XXX, _min = XXX] )"
+    return detail.exponential_autocorrelation_time_limit(timeseries, _max, _min)
+  print "Usage: exponential_autocorrelation_time(timeseries, [_from = XXX, _to = XXX | _max = XXX, _min = XXX] )"
 
 binning = "binning"
 uncorrelated = "uncorrelated"
 
-def error(obs, selector = "uncorrelated"):
+def error(timeseries, selector = uncorrelated):
+  "Calculates an error estimate of a timeseries. Can be invoked with 'uncorrelated' or 'binning'\n\
+timeseries: Any MCTimeseries or MCData object\n\
+uncorrelated (default): The error estimate when uncorrelated data is assumed\n\
+binning: The error estimate is calculated using a binning analysis.\n\
+returns: A float or numpyarray (depending on the dimension of the timeseries) with the error(s)"
   if selector == "binning":
-    return binning_error(obs)
+    return detail.binning_error(timeseries)
   if selector == "uncorrelated":
-    return uncorrelated_error(obs)
+    return detail.uncorrelated_error(timeseries)
 
