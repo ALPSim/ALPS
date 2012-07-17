4a1bf37a7ea71509798b1228941c53e2e86c98ad
gukel <gukel@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2012-07-17 15:02:54 +0000

    extract path to intel binaries from $CC instead of hardcoding. works for old and new CentOS installation on brutus.
    git-svn-id: svn://localhost:7744/trunk/alps@6304 3665984a-f8b0-4bb5-b30c-cdcaecbe7ced

--- config/brutus-intel12.cmake
+++ config/brutus-intel12.cmake
@@ -4,12 +4,12 @@
 #          http://www.boost.org/LICENSE_1_0.txt)
 #
 # We assume the following modules are loaded:
-#   module load hdf5 goto2 python/2.7.2 intel/12 open_mpi/1.5 cmake
+#   module load hdf5 goto2 python/2.7.2 intel/12 open_mpi/1.[56] cmake
 # Do NOT load module mkl, it is already included with intel/12.
 #
 # icpc option "-fast" enables "-static" which does not work well with cmake's
 # library search when there are dynamic libraries around. 
-# Therefore, in CMAKE_CXX_FLAGS_RELEASE we manually set allother options
+# Therefore, in CMAKE_CXX_FLAGS_RELEASE we manually set all other options
 # included in "-fast". 
 
 SET(CMAKE_CXX_COMPILER $ENV{CXX})
@@ -17,8 +17,12 @@ SET(CMAKE_C_COMPILER $ENV{CC})
 SET(CMAKE_Fortran_COMPILER $ENV{FC})
 SET(CMAKE_CXX_FLAGS_RELEASE "-ipo -O3 -no-prec-div -xHost -mkl -DNDEBUG -DBOOST_DISABLE_ASSERTS" CACHE STRING "" FORCE)
 SET(CMAKE_CXX_FLAGS_DEBUG "-g -mkl" CACHE STRING "" FORCE)
-SET(CMAKE_AR "/cluster/apps/intel/composerxe-2011.3.174/bin/intel64/xiar" CACHE STRING "" FORCE) 
-SET(CMAKE_LINKER "/cluster/apps/intel/composerxe-2011.3.174/bin/intel64/xild" CACHE STRING "" FORCE)
+
+STRING(REGEX REPLACE "^(.+)/icc$" "\\1" INTEL_BINDIR "${CMAKE_C_COMPILER}")
+#MESSAGE(STATUS "Intel binary directory is: ${INTEL_BINDIR}")
+
+SET(CMAKE_AR "${INTEL_BINDIR}/xiar" CACHE STRING "" FORCE) 
+SET(CMAKE_LINKER "${INTEL_BINDIR}/xild" CACHE STRING "" FORCE)
 
 SET(LAPACK_FOUND 1) # with -mkl we have lapack no matter what cmake thinks
 SET(HAVE_MKL 1)
