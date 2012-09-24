2e4d73f13ec9cfb6e6f1a1c0594983541d77daf9
gamperl <gamperl@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2012-09-24 12:13:29 +0000

    fixed #696
    git-svn-id: svn://localhost:7744/trunk/alps@6421 3665984a-f8b0-4bb5-b30c-cdcaecbe7ced

--- applications/dmrg/tebd/core/ObOps.f90
+++ applications/dmrg/tebd/core/ObOps.f90
@@ -1,18 +1,22 @@
 !    Copyright (C) 2010  M. L. Wall and L. D. Carr, Colorado School of Mines
 !    This file is part of OpenSourceTEBD.
 !
-!    This program is free software: you can redistribute it and/or modify
-!    it under the terms of the GNU General Public License as published by
-!    the Free Software Foundation, either version 3 of the License, or
-!    (at your option) any later version.
-
-!    This program is distributed in the hope that it will be useful,
-!    but WITHOUT ANY WARRANTY; without even the implied warranty of
-!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-!    GNU General Public License for more details.
-
-!    You should have received a copy of the GNU General Public License
-!    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+!    This software is part of the ALPS libraries, published under the ALPS
+!    Library License; you can use, redistribute it and/or modify it under
+!    the terms of the license, either version 1 or (at your option) any later
+!    version.
+!
+!    You should have received a copy of the ALPS Library License along with
+!    the ALPS Libraries; see the file LICENSE.txt. If not, the license is also
+!    available from http://alps.comp-phys.org/.
+!
+!    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+!    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+!    FITNESS FOR A PARTICULAR PURPOSE, TITLE AND NON-INFRINGEMENT. IN NO EVENT
+!    SHALL THE COPYRIGHT HOLDERS OR ANYONE DISTRIBUTING THE SOFTWARE BE LIABLE
+!    FOR ANY DAMAGES OR OTHER LIABILITY, WHETHER IN CONTRACT, TORT OR OTHERWISE,
+!    ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+!    DEALINGS IN THE SOFTWARE.
 MODULE ObOps
 !
 ! Purpose: Module to compute observables
