18c2904bf67479c8748bf77861aba2599d7d4664
mkoenz <mkoenz@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2012-10-17 21:48:56 +0000

    inserted second cmake flag ALPS_NGS_INCLUDE_TEST_AND_EXAMPLE, b/c tests and example should always compile while ALPS_NGS_USE_NEW_ALEA isn't fully integrated accumulator_set.hpp/cpp created
    Compiled and tested with: Ubuntu clang version 3.0 and Apple clang version 4.1
    Some tests didn't run
    
    
    git-svn-id: svn://localhost:7744/trunk/alps@6479 3665984a-f8b0-4bb5-b30c-cdcaecbe7ced

--- example/ngs/alea/custom_accum.hpp
+++ example/ngs/alea/custom_accum.hpp
@@ -55,27 +55,35 @@ struct myVector: public std::vector<int>
 typedef myVector custom_accum;
 
 
-// with this specialisation the detail::accumulator_wrapper knows, that custom_accum has a mean function
-template<> struct has_mean<custom_accum> 
+namespace alps
 {
-    enum{value = true};
-};
+    namespace alea
+    {
+        // with this specialisation the detail::accumulator_wrapper knows, that custom_accum has a mean function
+        template<> struct has_mean<custom_accum> 
+        {
+            enum{value = true};
+        };
 
 
-// all traits (has_X) that are not specialised are set to false 
-// unless custom_accum has a memberfunction that is called X() const
+        // all traits (has_X) that are not specialised are set to false 
+        // unless custom_accum has a memberfunction that is called X() const
 
-// the value_type trait must be specialised
-template<>
-struct value_type<custom_accum>
+        // the value_type trait must be specialised
+        template<>
+        struct value_type<custom_accum>
+        {
+            typedef int type;
+        };
+
+        // this is the free mean function for the custom_accum (return value via mean trait)
+        
+    }//end namespace alea
+}//end namespace alps
+
+alps::alea::mean_type<alps::alea::value_type<custom_accum>::type>::type mean(const custom_accum & arg)
 {
-    typedef int type;
-};
-
-// this is the free mean function for the custom_accum (return value via mean trait)
-mean_type<value_type<custom_accum>::type>::type mean(const custom_accum & arg)
-{
-    typedef mean_type<value_type<custom_accum>::type>::type mean_type;
+    typedef alps::alea::mean_type<alps::alea::value_type<custom_accum>::type>::type mean_type;
     
     return mean_type(std::accumulate(arg.begin(), arg.end(), 0))/arg.size();
 }
