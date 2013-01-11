96648da8e4d1037fa313295aee5b4e786232c730
rigarash <rigarash@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2013-01-11 06:43:11 +0000

    Allow linking to pre-built Boost.test Unit Test Framework (default disabled). This reduces compile time about 5% and the size of the build directory by 2%.
    git-svn-id: svn://localhost:7744/trunk/alps@6698 3665984a-f8b0-4bb5-b30c-cdcaecbe7ced

--- config/FindBoostForALPS.cmake
+++ config/FindBoostForALPS.cmake
@@ -2,7 +2,7 @@
 # Find Boost precompiled libraries or Boost source tree for ALPS
 #
 
-#  Copyright Ryo IGARASHI 2010, 2011.
+#  Copyright Ryo IGARASHI 2010, 2011, 2013.
 #  Distributed under the Boost Software License, Version 1.0.
 #      (See accompanying file LICENSE_1_0.txt or copy at
 #          http://www.boost.org/LICENSE_1_0.txt)
@@ -11,7 +11,8 @@
 # we use precompiled Boost libraries only when Boost_ROOT_DIR is not set.
 if (NOT Boost_ROOT_DIR)
 
-  set(Boost_ADDITIONAL_VERSIONS "1.48" "1.48.0" "1.47" "1.47.0")
+  # Old version of CMake (< 2.8.8) do not support for new Boost version
+  set(Boost_ADDITIONAL_VERSIONS "1.56.0" "1.56" "1.55.0" "1.55" "1.54.0" "1.54" "1.53.0" "1.53" "1.52.0" "1.52" "1.51.0" "1.51" "1.50.0" "1.50" "1.49.0" "1.49" "1.48.0" "1.48" "1.47.0" "1.47")
   # Debian and Ubuntu packages are multithreaded.
   # Commented out because default is ON.
   #  set(Boost_USE_MULTITHREADED ON)
@@ -27,14 +28,29 @@ if (NOT Boost_ROOT_DIR)
     message(STATUS "Looking for precompiled Boost libraries (version >= 1.48)")
 
     find_package(Boost 1.48.0 COMPONENTS mpi)
+    if (ALPS_LINK_BOOST_TEST)
+      set(UTF "unit_test_framework")
+    else (ALPS_LINK_BOOST_TEST)
+      set(UTF)
+    endif(ALPS_LINK_BOOST_TEST)
     if (Boost_FOUND)
-      find_package(Boost 1.48.0 COMPONENTS chrono date_time filesystem program_options python regex system serialization thread mpi)
+      find_package(Boost 1.48.0 COMPONENTS chrono date_time filesystem program_options python regex system serialization thread mpi ${UTF})
     else (Boost_FOUND)
-      find_package(Boost 1.48.0 COMPONENTS chrono date_time filesystem program_options python regex system serialization thread)
+      find_package(Boost 1.48.0 COMPONENTS chrono date_time filesystem program_options python regex system serialization thread ${UTF})
     endif (Boost_FOUND)
     if (APPLE AND NOT Boost_FOUND)
-      find_package(boost COMPONENTS chrono-mt date_time-mt filesystem-mt program_options-mt python-mt regex-mt system-mt serialization-mt thread-mt mpi-mt)
+      find_package(boost COMPONENTS chrono-mt date_time-mt filesystem-mt program_options-mt python-mt regex-mt system-mt serialization-mt thread-mt mpi-mt ${UTF}-mt)
     endif(APPLE AND NOT Boost_FOUND)
+    if (UTF AND Boost_FOUND)
+      # Unset ALPS_INSTALL_BOOST_TEST
+      # since Boost.Test dynamic library is already installed
+      unset(ALPS_INSTALL_BOOST_TEST)
+      # Add definitions needed to link Boost.test Unit Test Framework.
+      add_definitions(-DBOOST_TEST_DYN_LINK)
+      add_definitions(-DALPS_LINK_BOOST_TEST)
+    endif (UTF AND Boost_FOUND)
+    # Unset local variable
+    unset(UTF)
 
 endif(NOT Boost_ROOT_DIR)
 
