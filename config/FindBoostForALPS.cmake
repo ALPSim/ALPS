039df6bcd4db85b070384742cf1f81b06bef10b2
rigarash <rigarash@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2013-01-15 07:08:23 +0000

    Add Boost.timer (v2) detection and linking code.
    git-svn-id: svn://localhost:7744/trunk/alps@6703 3665984a-f8b0-4bb5-b30c-cdcaecbe7ced

--- config/FindBoostForALPS.cmake
+++ config/FindBoostForALPS.cmake
@@ -12,49 +12,58 @@
 if (NOT Boost_ROOT_DIR)
 
   # Old version of CMake (< 2.8.8) do not support for new Boost version
-  set(Boost_ADDITIONAL_VERSIONS "1.56.0" "1.56" "1.55.0" "1.55" "1.54.0" "1.54" "1.53.0" "1.53" "1.52.0" "1.52" "1.51.0" "1.51" "1.50.0" "1.50" "1.49.0" "1.49" "1.48.0" "1.48" "1.47.0" "1.47")
-  # Debian and Ubuntu packages are multithreaded.
-  # Commented out because default is ON.
-  #  set(Boost_USE_MULTITHREADED ON)
+  set(Boost_ADDITIONAL_VERSIONS "1.56.0" "1.56" "1.55.0" "1.55" "1.54.0" "1.54" "1.53.0" "1.53" "1.52.0" "1.52" "1.51.0" "1.51" "1.50.0" "1.50" "1.49.0" "1.49" "1.48.0" "1.48")
+  # Debian and Ubuntu packages are multithreaded by default but not on MacOSX.
+  # Explicitly set multithread flag.
+  set(Boost_USE_MULTITHREADED ON)
   # If you want to use static libraries, uncomment this option.
   #  set(Boost_USE_STATIC_LIBS ON)
   # Debug flag for FindBoost.cmake
   #  set(Boost_DEBUG TRUE)
 
+  message(STATUS "Looking for precompiled Boost libraries (version >= 1.48)")
+
   # We do not require Boost.MPI, therefore check whether Boost.MPI exists
   # before actual find_package for Boost.
   # - Ubuntu 10.10 does not have Boost.MPI package.
-    # Boost 1.47.0 or later is not yet supported by FindBoost.cmake (CMake 2.8.5).
-    message(STATUS "Looking for precompiled Boost libraries (version >= 1.48)")
-
-    find_package(Boost 1.48.0 COMPONENTS mpi)
-    if (ALPS_LINK_BOOST_TEST)
-      set(UTF "unit_test_framework")
-    else (ALPS_LINK_BOOST_TEST)
-      set(UTF)
-    endif(ALPS_LINK_BOOST_TEST)
+  set(MPI)
+  find_package(Boost 1.48.0 COMPONENTS mpi)
+  if (Boost_FOUND)
+    set(MPI mpi)
+  endif (Boost_FOUND)
+  # Linking to Boost.test Unit Test Framework is optional
+  set(UTF)
+  if (ALPS_LINK_BOOST_TEST)
+    find_package(Boost 1.48.0 COMPONENTS unit_test_framework)
     if (Boost_FOUND)
-      find_package(Boost 1.48.0 COMPONENTS chrono date_time filesystem program_options python regex system serialization thread mpi ${UTF})
-    else (Boost_FOUND)
-      find_package(Boost 1.48.0 COMPONENTS chrono date_time filesystem program_options python regex system serialization thread ${UTF})
-    endif (Boost_FOUND)
-    if (APPLE AND NOT Boost_FOUND)
-      find_package(boost COMPONENTS chrono-mt date_time-mt filesystem-mt program_options-mt python-mt regex-mt system-mt serialization-mt thread-mt mpi-mt ${UTF}-mt)
-    endif(APPLE AND NOT Boost_FOUND)
-    if (UTF AND Boost_FOUND)
+      set(UTF unit_test_framework)
       # Unset ALPS_INSTALL_BOOST_TEST
       # since Boost.Test dynamic library is already installed
       unset(ALPS_INSTALL_BOOST_TEST)
       # Add definitions needed to link Boost.test Unit Test Framework.
       add_definitions(-DBOOST_TEST_DYN_LINK)
       add_definitions(-DALPS_LINK_BOOST_TEST)
-    endif (UTF AND Boost_FOUND)
-    # Unset local variable
-    unset(UTF)
+    endif (Boost_FOUND)
+  endif(ALPS_LINK_BOOST_TEST)
+  # Linking to Boost.timer (v2) is not used (and thus optional) as of ALPS 2.1.
+  # However, Boost.timer (v2) is built and linked when boost is built together.
+  # Therefore, When Boost.timer (v2) is found, link ALPS against it.
+  set(TIMER)
+  find_package(Boost 1.48.0 COMPONENTS timer)
+  if (Boost_FOUND)
+    set(TIMER timer)
+  endif (Boost_FOUND)
+  # The final library finding for precompiled Boost.
+  find_package(Boost 1.48.0 COMPONENTS chrono date_time filesystem program_options python regex system serialization thread ${MPI} ${UTF} ${TIMER})
+  # Unset local variable
+  unset(MPI)
+  unset(UTF)
+  unset(TIMER)
 
 endif(NOT Boost_ROOT_DIR)
 
 # Boost_FOUND is set only when FindBoost.cmake succeeds.
+# if not, build Boost libraries from source.
 if (NOT Boost_FOUND)
   message(STATUS "Looking for Boost Source")
   find_package(BoostSrc)
