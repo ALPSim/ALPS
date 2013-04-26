8563399d846d059f6a0e41414f8cc131f4415c56
tamama <tamama@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2013-04-26 16:25:51 +0000

    scripts/tools.py for better thermalization wrapper
    git-svn-id: svn://localhost:7744/trunk/alps@6853 3665984a-f8b0-4bb5-b30c-cdcaecbe7ced

--- applications/qmc/dwa/scripts/tools.py
+++ applications/qmc/dwa/scripts/tools.py
@@ -1,7 +1,26 @@
-def thermalized(parmfiles, observables):
-  if isinstance(parmfiles, str):
-    parmfiles = [parmfiles];
+import numpy;
+import scipy;
+import pyalps;
+
+def thermalized(h5_outfile, observables, tolerance=0.01, includeLog=False):
   if isinstance(observables, str):
     observables = [observables];
 
-  return [parmfiles, observables];
+  results = [];
+  for observable in observables:
+    timeseries = pyalps.hdf5.iArchive(h5_outfile).read("/simulation/results/" + observable)['timeseries']['data']; 
+    mean = timeseries.mean();
+
+    index = scipy.linspace(0, timeseries.size-1, timeseries.size);
+    timeseries = scipy.polyval(scipy.polyfit(index, timeseries, 1), index);  # timeseries get fitted
+
+    percentage_increment = (timeseries[-1] - timeseries[0])/mean;
+    result = abs(percentage_increment) < tolerance;
+
+    if not includeLog:
+      results.append(result);
+    else:
+      results.append({'observable': observable, 'percentage_increment' : percentage_increment, 'thermalized': result})
+
+  return results;
+
