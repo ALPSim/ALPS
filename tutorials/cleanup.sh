6fd96c501247b4b6bc2d6908c96cc07fef6a8f8d
dolfim <dolfim@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2014-01-28 15:02:31 +0000

    mps-06 tutorial with movie of magnetization
    git-svn-id: svn://localhost:7744/trunk/alps@7316 3665984a-f8b0-4bb5-b30c-cdcaecbe7ced

--- tutorials/cleanup.sh
+++ tutorials/cleanup.sh
@@ -4,6 +4,6 @@
 #      (See accompanying file LICENSE_1_0.txt or copy at
 #          http://www.boost.org/LICENSE_1_0.txt)
 
-rm */parm*xml */*clone* */*.xsl */*.run* */*.dat */*.h5* */*out.xml */*in.xml */selfenergy*
-rm parm*xml *clone* *.xsl *.run* *.dat *.h5* *out.xml *in.xml 
+rm */parm*xml */*clone* */*.xsl */*.run* */*.dat */*.h5* */*out.xml */*.mp4 */*in.xml */selfenergy*
+rm parm*xml *clone* *.xsl *.run* *.dat *.h5* *out.xml *in.xml
 rm -r */*.chkp
