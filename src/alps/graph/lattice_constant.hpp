a181522bab01f35a756ae462bd370ab6f8ac8d4c
hehn <hehn@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2015-06-18 21:43:35 +0000

    added overloads for more efficient lattice constant calculations with edge color symmetries
    git-svn-id: svn://localhost:7744/trunk/alps@7585 3665984a-f8b0-4bb5-b30c-cdcaecbe7ced

--- src/alps/graph/lattice_constant.hpp
+++ src/alps/graph/lattice_constant.hpp
@@ -63,6 +63,29 @@ namespace alps {
         }
 
         template<typename Subgraph, typename Graph, typename Lattice> std::size_t lattice_constant(
+              Subgraph const & S
+            , Graph const & G
+            , Lattice const & L
+            , typename alps::lattice_traits<Lattice>::cell_descriptor c
+            , std::vector<alps::type_type> const& edge_color_mapping
+        ) {
+            typename partition_type<Subgraph>::type subgraph_orbit = boost::get<2>(canonical_properties(S));
+
+            typename alps::lattice_traits<Lattice>::size_type const cell_id = index(c, L);
+            std::size_t unit_cell_size = num_vertices(alps::graph::graph(unit_cell(L)));
+            std::vector<typename boost::graph_traits<Graph>::vertex_descriptor> V;
+            for(unsigned v = 0; v < unit_cell_size; ++v)
+                V.push_back(cell_id * unit_cell_size + v);
+
+            detail::vertex_equal_simple<Subgraph> vertex_equal;
+            detail::edge_equal_mapped_colors<Subgraph> edge_equal(edge_color_mapping);
+            boost::false_type no_argument;
+            detail::lattice_constant_inserter<Subgraph, boost::false_type>    register_embedding(S, G, L, subgraph_orbit, no_argument);
+            detail::lattice_constant_impl(S, G, V, subgraph_orbit, vertex_equal, edge_equal, register_embedding);
+            return register_embedding.get_count();
+        }
+
+        template<typename Subgraph, typename Graph, typename Lattice> std::size_t lattice_constant(
               alps::numeric::matrix<unsigned int> & lw
             , Subgraph const & S
             , Graph const & G
@@ -81,6 +104,30 @@ namespace alps {
 
             detail::vertex_equal_simple<Subgraph> vertex_equal;
             detail::edge_equal_simple<Subgraph>   edge_equal;
+            detail::lattice_constant_inserter<Subgraph, alps::numeric::matrix<unsigned int> >    register_embedding(S, G, L, subgraph_orbit, lw, b);
+            detail::lattice_constant_impl(S, G, V, subgraph_orbit, vertex_equal, edge_equal, register_embedding);
+            return register_embedding.get_count();
+        }
+        template<typename Subgraph, typename Graph, typename Lattice> std::size_t lattice_constant(
+              alps::numeric::matrix<unsigned int> & lw
+            , Subgraph const & S
+            , Graph const & G
+            , Lattice const & L
+            , typename alps::lattice_traits<Lattice>::cell_descriptor c
+            , typename boost::graph_traits<Subgraph>::vertex_descriptor b
+            , typename partition_type<Subgraph>::type const & subgraph_orbit
+            , std::vector<alps::type_type> const& edge_color_mapping
+        ) {
+            assert(get<alps::graph::partition>(canonical_properties(S,b)) == subgraph_orbit);
+
+            typename alps::lattice_traits<Lattice>::size_type const cell_id = index(c, L);
+            std::size_t unit_cell_size = num_vertices(alps::graph::graph(unit_cell(L)));
+            std::vector<typename boost::graph_traits<Graph>::vertex_descriptor> V;
+            for(unsigned v = 0; v < unit_cell_size; ++v)
+                V.push_back(cell_id * unit_cell_size + v);
+
+            detail::vertex_equal_simple<Subgraph> vertex_equal;
+            detail::edge_equal_mapped_colors<Subgraph> edge_equal(edge_color_mapping);
             detail::lattice_constant_inserter<Subgraph, alps::numeric::matrix<unsigned int> >    register_embedding(S, G, L, subgraph_orbit, lw, b);
             detail::lattice_constant_impl(S, G, V, subgraph_orbit, vertex_equal, edge_equal, register_embedding);
             return register_embedding.get_count();
